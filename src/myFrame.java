8107a76c2f8c321d06841601b630f63db997dd7c
Helias <stefano520@hotmail.it>
2012-09-19 23:05:03 +0200

    Added an icon to the menuitem exit and added the shortcut to its, added the textarea of the editor

--- src/myFrame.java
+++ src/myFrame.java
@@ -5,9 +5,10 @@ public class myFrame extends JFrame
 {
 	public static myFrame frame = new myFrame();
 	private JMenuBar myMenuBar = new JMenuBar();
-	
+
 	private JMenu File = new JMenu();
 	private JMenu Tools = new JMenu();
+
 
 	// File
 	private JMenuItem ImportHTML = new JMenuItem();
@@ -15,31 +16,36 @@ public class myFrame extends JFrame
 	private JMenuItem SaveHTML = new JMenuItem();
 	private JMenuItem SaveBBCode = new JMenuItem();
 	private JMenuItem Exit = new JMenuItem();
-	
+
 	// Tools
 	private JMenuItem Palette = new JMenuItem();
 	
+	// TextArea
+	private JTextArea TextArea = new JTextArea();
+
 	private myFrame()	{ initFrame(); }
 	private void initFrame()
 	{
 		Toolkit myKit=Toolkit.getDefaultToolkit();
 		Dimension myScreenSize = myKit.getScreenSize();
-		
+
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setTitle("HTML-BBCode converter");
 		this.setVisible(true);
 		this.setSize(myScreenSize.width/2, myScreenSize.height/2);
 		this.setLocation(myScreenSize.width/4, myScreenSize.height/4);
-		
+
 		File.setText("File");
 		Tools.setText("Tools");
-		
+
 		ImportHTML.setText("Import HTML..");
 		ImportBBCode.setText("Import BBCode..");
 		SaveHTML.setText("Save HTML");
 		SaveBBCode.setText("Save BBCode");
 		Exit.setText("Exit");
-		
+
+		Exit.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_Q, java.awt.event.InputEvent.CTRL_MASK));
+		Exit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Exit.png")));
 		Exit.addActionListener(new java.awt.event.ActionListener()
 		{
 			public void actionPerformed(java.awt.event.ActionEvent evt)
@@ -47,17 +53,23 @@ public class myFrame extends JFrame
 				System.exit(0);
 			}
 		});
+		
+		TextArea.setColumns(20);
+		TextArea.setRows(5);
+
 
 		this.setJMenuBar(myMenuBar);
-		
+
 		myMenuBar.add(File);
 		myMenuBar.add(Tools);
-		
+
 		File.add(ImportHTML);
 		File.add(ImportBBCode);
 		File.add(SaveHTML);
 		File.add(SaveBBCode);
 		File.add(Exit);
+
+		this.add(TextArea);
 	}
 	
 	public static myFrame getFrame()	{ return frame; }
