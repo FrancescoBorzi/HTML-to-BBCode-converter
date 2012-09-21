acc52bd1361bbe440a812692c4346a5ad40dac1c
ShinDarth <shin.darth@hotmail.it>
2012-09-21 11:11:20 +0200

    JFrame recoded

--- src/myFrame.java
+++ src/myFrame.java
@@ -1,101 +1,225 @@
-import javax.swing.*;
-import java.awt.*;
+public class myFrame extends javax.swing.JFrame {
 
-public class myFrame extends JFrame
-{
-	public static myFrame frame = new myFrame();
-	private JMenuBar myMenuBar = new JMenuBar();
+    private static myFrame frame = new myFrame();
+    
+    public static myFrame getFrame()
+    {
+        return frame;
+    }
+    
+    private myFrame()
+    {
+        initComponents();
+        this.setVisible(true);
+    }
 
-	private JMenu File = new JMenu();
-	private JMenu Tools = new JMenu();
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
 
-	// File
-	private JMenuItem ImportHTML = new JMenuItem();
-	private JMenuItem ImportBBCode = new JMenuItem();
-	private JMenuItem SaveHTML = new JMenuItem();
-	private JMenuItem SaveBBCode = new JMenuItem();
-	private JMenuItem Exit = new JMenuItem();
+        jPanel1 = new javax.swing.JPanel();
+        convert1 = new javax.swing.JButton();
+        check1 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jPanel2 = new javax.swing.JPanel();
+        convert2 = new javax.swing.JButton();
+        check2 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea2 = new javax.swing.JTextArea();
 
-	// Tools
-	private JMenuItem Palette = new JMenuItem();
-	
-	// Panels
-	private JPanel panelHTMLbuttons = new JPanel();
-	private JPanel panelBBCodebuttons = new JPanel();
-	
-	// Texts
-	private JTextArea textHTML = new JTextArea(10, 10);
-	private JTextArea textBBCode = new JTextArea(10, 10);
-	
-	// Buttons
-	private JButton check1 = new JButton("Check code");
-	private JButton check2 = new JButton("Check code");
-	private JButton convert1 = new JButton("Convert to BBCode");
-	private JButton convert2 = new JButton("Convert to HTML");
-	
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-	// TextArea
+        convert1.setText("Convert to BBCode");
+        convert1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                convert1ActionPerformed(evt);
+            }
+        });
 
-	private myFrame()	{ initFrame(); }
-	private void initFrame()
-	{
-		Toolkit myKit=Toolkit.getDefaultToolkit();
-		Dimension myScreenSize = myKit.getScreenSize();
+        check1.setText("Check");
+        check1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                check1ActionPerformed(evt);
+            }
+        });
 
-		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		this.setTitle("HTML-BBCode converter");
-		this.setVisible(true);
-		this.setSize(myScreenSize.width/2, myScreenSize.height/2);
-		this.setLocation(myScreenSize.width/4, myScreenSize.height/4);
+        jLabel1.setText("HTML");
 
-		File.setText("File");
-		Tools.setText("Tools");
+        jTextArea1.setColumns(20);
+        jTextArea1.setRows(5);
+        jScrollPane1.setViewportView(jTextArea1);
 
-		ImportHTML.setText("Import HTML..");
-		ImportBBCode.setText("Import BBCode..");
-		SaveHTML.setText("Save HTML");
-		SaveBBCode.setText("Save BBCode");
-		Exit.setText("Exit");
-		Palette.setText("Palette");
-		
-		this.setJMenuBar(myMenuBar);
-		this.add(panelHTMLbuttons, BorderLayout.LINE_START);
-		this.add(panelBBCodebuttons, BorderLayout.LINE_END);
-		
-		panelHTMLbuttons.add(check1);
-		panelHTMLbuttons.add(convert1);
-		panelBBCodebuttons.add(check2);
-		panelBBCodebuttons.add(convert2);
-		
+        org.jdesktop.layout.GroupLayout jPanel1Layout = new org.jdesktop.layout.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(jPanel1Layout.createSequentialGroup()
+                        .add(8, 8, 8)
+                        .add(check1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                        .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                            .add(jLabel1)
+                            .add(convert1))
+                        .add(10, 10, 10))
+                    .add(jScrollPane1))
+                .addContainerGap())
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .add(jLabel1)
+                .add(12, 12, 12)
+                .add(jPanel1Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(convert1)
+                    .add(check1))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(jScrollPane1, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, 234, Short.MAX_VALUE))
+        );
 
-		/*Exit.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_Q, java.awt.event.InputEvent.CTRL_MASK));
-		Exit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Exit.png")));
-		Exit.addActionListener(new java.awt.event.ActionListener()
-		{
-			public void actionPerformed(java.awt.event.ActionEvent evt)
-			{
-				System.exit(0);
-			}
-		});
+        convert2.setText("Convert to HTML");
+        convert2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                convert2ActionPerformed(evt);
+            }
+        });
 
-		TextArea.setLineWrap(true);*/
+        check2.setText("Check");
+        check2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                check2ActionPerformed(evt);
+            }
+        });
 
-		this.setJMenuBar(myMenuBar);
+        jLabel2.setText("BBCode");
 
-		myMenuBar.add(File);
-		myMenuBar.add(Tools);
+        jTextArea2.setColumns(20);
+        jTextArea2.setRows(5);
+        jScrollPane2.setViewportView(jTextArea2);
 
-		File.add(ImportHTML);
-		File.add(ImportBBCode);
-		File.add(SaveHTML);
-		File.add(SaveBBCode);
-		File.add(Exit);
-		
-		Tools.add(Palette);
+        org.jdesktop.layout.GroupLayout jPanel2Layout = new org.jdesktop.layout.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .add(jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(jScrollPane2)
+                    .add(jPanel2Layout.createSequentialGroup()
+                        .add(6, 6, 6)
+                        .add(check2, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                        .add(convert2)
+                        .add(9, 9, 9))
+                    .add(org.jdesktop.layout.GroupLayout.TRAILING, jPanel2Layout.createSequentialGroup()
+                        .add(0, 0, Short.MAX_VALUE)
+                        .add(jLabel2)
+                        .add(94, 94, 94)))
+                .addContainerGap())
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(jPanel2Layout.createSequentialGroup()
+                .addContainerGap()
+                .add(jLabel2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 22, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(jPanel2Layout.createParallelGroup(org.jdesktop.layout.GroupLayout.BASELINE)
+                    .add(convert2)
+                    .add(check2))
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.RELATED)
+                .add(jScrollPane2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, 236, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(50, Short.MAX_VALUE))
+        );
 
-		/* this.add(Scroll); */
-	}
+        org.jdesktop.layout.GroupLayout layout = new org.jdesktop.layout.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(layout.createSequentialGroup()
+                .addContainerGap()
+                .add(jPanel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(org.jdesktop.layout.LayoutStyle.UNRELATED)
+                .add(jPanel2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+            .add(layout.createSequentialGroup()
+                .addContainerGap()
+                .add(layout.createParallelGroup(org.jdesktop.layout.GroupLayout.LEADING)
+                    .add(jPanel2, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE)
+                    .add(jPanel1, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE, org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, org.jdesktop.layout.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(org.jdesktop.layout.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
 
-	public static myFrame getFrame()	{ return frame; }
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void check1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_check1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_check1ActionPerformed
+
+    private void convert2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_convert2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_convert2ActionPerformed
+
+    private void check2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_check2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_check2ActionPerformed
+
+    private void convert1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_convert1ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_convert1ActionPerformed
+
+    public static void main(String args[]) {
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /*
+         * If Nimbus (introduced in Java SE 6) is not available, stay with the
+         * default look and feel. For details see
+         * http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(myFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(myFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(myFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(myFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        java.awt.EventQueue.invokeLater(new Runnable() {
+
+            public void run() {
+                getFrame();
+            }
+        });
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton check1;
+    private javax.swing.JButton check2;
+    private javax.swing.JButton convert1;
+    private javax.swing.JButton convert2;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JPanel jPanel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextArea jTextArea2;
+    // End of variables declaration//GEN-END:variables
 }
-
