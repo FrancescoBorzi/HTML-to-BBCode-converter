d05e204fbc5e4afabcb4fb99134362bab9f35937
ShinDarth <shin.darth@hotmail.it>
2012-09-25 11:08:14 +0200

    Implement links check in HTML textArea

--- src/Handler.java
+++ src/Handler.java
@@ -1,4 +1,6 @@
 import java.util.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public interface Handler
 {
@@ -44,6 +46,17 @@ class HTML implements Handler
             javax.swing.JOptionPane.showMessageDialog(null, "There are some errors in your code, please check it!", "HTML: Error", javax.swing.JOptionPane.ERROR_MESSAGE);
             return;
         }
+        
+        String links[] = URLchecks.findHTMLlinks(input);
+        
+        for (int i = 0; i < links.length; i++)
+            if (!URLchecks.exists(links[i]))
+            {
+                javax.swing.JOptionPane.showMessageDialog(null, "One or more links may be invalid.", "URL check", javax.swing.JOptionPane.ERROR_MESSAGE);
+                return;
+            }
+                
+                
         
         javax.swing.JOptionPane.showMessageDialog(null, "Everything's ok!", "HTML: OK", javax.swing.JOptionPane.INFORMATION_MESSAGE);
     }
@@ -114,5 +127,68 @@ class Context
     {
         return handlerOfBBcode;
     }
+}
+
+class URLchecks
+{
+    public static boolean exists(String URLName)
+    {
+        try 
+        {
+            HttpURLConnection.setFollowRedirects(false);
+            HttpURLConnection con = (HttpURLConnection) new URL(URLName).openConnection();
+            con.setRequestMethod("HEAD");
+            return (con.getResponseCode() == HttpURLConnection.HTTP_OK);
+        }
+        catch (Exception e)
+        {
+            return false;
+        }
+    }
     
+    public static String[] findHTMLlinks(String input)
+    {
+        String[] links;
+        
+        int count = 0;
+        
+        for (int i = 0; i < input.length()-5; i++)
+        {
+            if (
+                    input.substring(i, i+5).compareTo("src='") == 0 ||
+                    input.substring(i, i+5).compareTo("src=\"") == 0 ||
+                    input.substring(i, i+5).compareTo("ref='") == 0 ||
+                    input.substring(i, i+5).compareTo("ref=\"") == 0
+                    )
+                count++;
+        }
+        
+        links = new String[count];
+        int position = 0;
+        
+        for (int i = 0; i < count; i++)
+        {
+            for (; position < input.length()-5; position++)
+            {
+                if (
+                    input.substring(position, position+5).compareTo("src='") == 0 ||
+                    input.substring(position, position+5).compareTo("src=\"") == 0 ||
+                    input.substring(position, position+5).compareTo("ref='") == 0 ||
+                    input.substring(position, position+5).compareTo("ref=\"") == 0
+                    )
+                {
+                    position = position + 5;
+                    int pos2 = position + 1;
+                    while (input.charAt(pos2) != '\"' && input.charAt(pos2) != '\'')
+                        pos2++;
+                    
+                    links[i] = input.substring(position, pos2);
+                    position = pos2;
+                    break;
+                }
+            }
+        }
+          
+        return links;
+    }
 }
